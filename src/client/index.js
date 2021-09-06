15ef63ed0018bc23d466a931a6924353eba71ee4
yasmine <55956540+semo22@users.noreply.github.com>
2021-09-06 21:46:14 +0200

    Add files via upload

---
+++ src/client/index.js
@@ -0,0 +1,12 @@
+import { handleSubmit } from './js/formHandler'
+
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+
+export{
+    handleSubmit
+}
+alert("I EXIST")
+console.log("CHANGE!!");
